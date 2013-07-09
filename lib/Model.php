d03296287c466ab3b5f9627ccc09f65255cb98ee
aimed <revdemiat@gmail.com>
2013-07-09 16:50:17 +0200

    whitespaces

--- lib/Model.php
+++ lib/Model.php
@@ -3,72 +3,72 @@
 class Model extends Injector {
 
 
-    /**
-     * Model table
-     */
-    public $table;
+	/**
+	 * Model table
+	 */
+	public $table;
 
 
-    /**
-     * Model attributes that are accessible via get
-     */
-    public $attributes;
-	
-	
+	/**
+	 * Model attributes that are accessible via get
+	 */
+	public $attributes;
+
+
 	/**
 	 * Model definition
 	 */
 	public $define;
-	
-	
+
+
 	/**
 	 * Primary Key
 	 */
 	protected $__primaryKey;
-	
-	
+
+
 	/**
 	 * List of keys
 	 */
-    protected $__keys;
-	
-	
+	protected $__keys;
+
+
 	/**
 	 * List of models belonging to this model
 	 */
 	protected $__childModels;
-	
-    
-    /**
-     * Stores modified attributes
-     */
-    private $_modified = array();
-    
-    
-    /**
-     * Constructor
-     */
-    public function __construct ($initial = null) {
-                
+
+
+	/**
+	 * Stores modified attributes
+	 */
+	private $_modified = array();
+
+
+	/**
+	 * Constructor
+	 */
+	public function __construct ($initial = null) {
+            
 		$this->registerWildcardFunction("findBy");
-        $this->registerWildcardFunction("findAllBy");
-        
-        // handle model definition
-        if (!empty($this->define))
+	    $this->registerWildcardFunction("findAllBy");
+    
+	    // handle model definition
+	    if (!empty($this->define))
 		{
-    		foreach ($this->define as $key => $val) {
-    			$this->__handleDefinition($key, $val);
-    			$this->attributes[] = is_numeric($key) ? $val : $key;
-    		}
+			foreach ($this->define as $key => $val) {
+				$this->__handleDefinition($key, $val);
+				$this->attributes[] = is_numeric($key) ? $val : $key;
+			}
 		}
-		
+	
 		// set default table name
 		if (empty($this->table)) $this->table = $this->camelcaseToUnderscore(get_class($this)) . "s";
-		
-        parent::__construct($initial);
-    }
 	
-	
+	    parent::__construct($initial);
+	}
+
+
 	/**
 	 * Handels a model definition
 	 *
@@ -76,11 +76,11 @@ class Model extends Injector {
 	 * @param Mixed $definitionValue
 	 */
 	protected function __handleDefinition ($definitionKey, $definitionValue) {
-	
+
 		if (is_numeric($definitionKey)) return; // is key is numeric, we want to do nothing		
 		if (is_string($definitionValue)) $definitionValue = array($definitionValue);
 		if (!is_array($definitionValue)) return;
-		
+	
 		foreach ($definitionValue as $key => $value) {
 			switch ($definitionValue) {
 				case "key": $this->__setKey($definitionKey); break;
@@ -89,8 +89,8 @@ class Model extends Injector {
 			}
 		}
 	}
-    
-	
+
+
 	/**
 	 * Primary key setter
 	 *
@@ -99,8 +99,8 @@ class Model extends Injector {
 	protected function __setPrimaryKey ($name) {
 		$this->__primaryKey = $name;
 	}
-	
-	
+
+
 	/**
 	 * Key setter
 	 *
@@ -109,19 +109,19 @@ class Model extends Injector {
 	protected function __setKey ($name) {
 		$this->__keys[] = $name;
 	}
-	
-	
+
+
 	/**
 	 * Model setter
 	 *
 	 * @param String $name
 	 */
 	protected function __setChildModel ($column, $model = null) {
-		if (!$model) $model = $this->underscoreToCamelcase($column); //@TODO: underscoreToCamelcase
+		if (!$model) $model = $this->underscoreToCamelcase($column);
 		$this->__childModels[$column] = $model;
 	}
-	
-	
+
+
 	/**
 	 * Checks if attribute is a child model
 	 *
@@ -131,132 +131,137 @@ class Model extends Injector {
 	protected function __isChildModel ($name) {
 		return array_key_exists($this->__childModels, $name);
 	}
-	
-    
-    /**
-     * Sets attributes
-     * if the key is an attribute, the result gets passed
-     * to the modified array and will be saved to the db 
-     *
-     * @param String $key
-     * @param Mixed $value
-     * @return Mixed Value
-     */
-    public function __set ($key, $value) {
-        $value = parent::__set($key, $value);
-        if ($this->hasAttribute($key)) $this->_modified[$key] = $value;
-        return $value;
-    }
-    
-    
-    /**
-     * Defaults hooks
-     */
-    public function beforeFind () {}
-    public function afterFind () {}
-    public function beforeInsert () {}
-    public function afterInsert () {}
-    public function beforeUpdate () {}
-    public function afterUpdate () {}
-    public function beforeDelete () {}
-    public function afterDelete () {}
-    public function onError () {}
 
 
-    /**
-     * Checks entity has attribute
-     *
-     * @param String $name
-     * @return Bool Is attribute
-     */
-    public function hasAttribute ($name) {
-        return in_array($name, $this->attributes);
-    }
-    
-    
-    /**
-     * Default attribute getter
-     *
-     * @return Array Attributes
-     */
-    public function find ($where) {
-        $this->beforeFind();
-        
-        $fetched = DB::grabOne($this->table, $this->attributes, $where);
-        $this->store($fetched);
-        
-        $this->afterFind();
-        
-        return $fetched;
-    }
-    
-    
-    /**
-     * Updates the model
-     *
-     * @param Array $where
-     * @return Integer affected rows
-     */
-    public function update ($where) {
-        if (empty($this->_modified)) return 0;
-                
-        DB::update($this->table, $this->_modified, $where);
-        $this->_modified = array();
-        
-        $this->afterUpdate();
-        return DB::affectedRows();
-    }
-    
-    
-    /**
-     * Inserts the model
-     *
-     * @param Array $where
-     * @return Mixed insert id
-     */
-    public function insert () {
-        $this->beforeInsert();
-        
-        $id = DB::insert($this->table, $this->_modified);
-        $this->_modified = array();
+	/**
+	 * 
+	 */
 
-        if (DB::affectedRows() === 0) $this->onError();         
 
-        $this->afterInsert();
-        
-        return $id;
-    }
+	/**
+	 * Sets attributes
+	 * if the key is an attribute, the result gets passed
+	 * to the modified array and will be saved to the db 
+	 *
+	 * @param String $key
+	 * @param Mixed $value
+	 * @return Mixed Value
+	 */
+	public function __set ($key, $value) {
+	    $value = parent::__set($key, $value);
+	    if ($this->hasAttribute($key)) $this->_modified[$key] = $value;
+	    return $value;
+	}
+
+
+	/**
+	 * Defaults hooks
+	 */
+	public function beforeFind () {}
+	public function afterFind () {}
+	public function beforeInsert () {}
+	public function afterInsert () {}
+	public function beforeUpdate () {}
+	public function afterUpdate () {}
+	public function beforeDelete () {}
+	public function afterDelete () {}
+	public function onError () {}
+
+
+	/**
+	 * Checks entity has attribute
+	 *
+	 * @param String $name
+	 * @return Bool Is attribute
+	 */
+	public function hasAttribute ($name) {
+	    return in_array($name, $this->attributes);
+	}
+
+
+	/**
+	 * Default attribute getter
+	 *
+	 * @return Array Attributes
+	 */
+	public function find ($where) {
+	    $this->beforeFind();
     
+	    $fetched = DB::grabOne($this->table, $this->attributes, $where);
+	    $this->store($fetched);
     
-    /**
-     * Deletes the object
-     *
-     * @return Bool Success
-     */
-    public function delete ($where) {
-        $this->beforeDelete();
-        
-        DB::delete($this->table, $where);
-        
-        $this->afterDelete();
-        
-        return DB::affectedRows() > 0;
-    }
+	    $this->afterFind();
     
+	    return $fetched;
+	}
+
+
+	/**
+	 * Updates the model
+	 *
+	 * @param Array $where
+	 * @return Integer affected rows
+	 */
+	public function update ($where) {
+	    if (empty($this->_modified)) return 0;
+            
+	    DB::update($this->table, $this->_modified, $where);
+	    $this->_modified = array();
     
-    /**
-     * Converts camel case to underscore name convention
-     * 
-     * @param String $s
-     * @return String Converted string
-     */
-    protected function camelcaseToUnderscore ($s) {
-        return preg_replace_callback("/[A-Z]+/", function ($s) { 
+	    $this->afterUpdate();
+	    return DB::affectedRows();
+	}
+
+
+	/**
+	 * Inserts the model
+	 *
+	 * @param Array $where
+	 * @return Mixed insert id
+	 */
+	public function insert () {
+	    $this->beforeInsert();
+    
+	    $id = DB::insert($this->table, $this->_modified);
+	    $this->_modified = array();
+
+	    if (DB::affectedRows() === 0) $this->onError();         
+
+	    $this->afterInsert();
+    
+	    return $id;
+	}
+
+
+	/**
+	 * Deletes the object
+	 *
+	 * @return Bool Success
+	 */
+	public function delete ($where) {
+	    $this->beforeDelete();
+    
+	    DB::delete($this->table, $where);
+    
+	    $this->afterDelete();
+    
+	    return DB::affectedRows() > 0;
+	}
+
+
+	/**
+	 * Converts camel case to underscore name convention
+	 * 
+	 * @param String $s
+	 * @return String Converted string
+	 */
+	protected function camelcaseToUnderscore ($s) {
+	    return preg_replace_callback("/[A-Z]+/", function ($s) { 
 			return "_" . strtolower($s[0]); 
 		}, lcfirst($s));
-    }
-	
-	
+	}
+
+
 	/**
 	 * Converts underscore to camelcase
 	 *
@@ -268,39 +273,39 @@ class Model extends Injector {
 			return strtoupper(substr($k[0],1));
 		}, $s));
 	}
-    
-    
-    /**
-     * Finds a model by given arguments
-     *
-     * @param String $ops
-     * @param Array $args
-     */    
-    public function findBy ($ops, $args) {
-        $this->beforeFind();
-        
-        $ops = $this->camelcaseToUnderscore($ops);
-        $query = QB::select()->from($this->table)->where($ops,"=",$args[0]);
 
-        $result = DB::row($query,$query->data);
-        $this->store($result);
-        
-        $this->afterFind();
-        
-        return $result;
-    }
+
+	/**
+	 * Finds a model by given arguments
+	 *
+	 * @param String $ops
+	 * @param Array $args
+	 */    
+	public function findBy ($ops, $args) {
+	    $this->beforeFind();
     
+	    $ops = $this->camelcaseToUnderscore($ops);
+	    $query = QB::select()->from($this->table)->where($ops,"=",$args[0]);
+
+	    $result = DB::row($query,$query->data);
+	    $this->store($result);
     
-    /**
-     * Finds a model by given arguments
-     *
-     * @param String $ops
-     * @param Array $args
-     */    
-    public function findAllBy ($ops, $args) {
-        $ops = $this->camelcaseToUnderscore($ops);
-        $query = QB::select()->from($this->table)->where($ops,"=",$args[0]);
-        
-        return DB::fetch($query,$query->data);
-    }
+	    $this->afterFind();
+    
+	    return $result;
+	}
+
+
+	/**
+	 * Finds a model by given arguments
+	 *
+	 * @param String $ops
+	 * @param Array $args
+	 */    
+	public function findAllBy ($ops, $args) {
+	    $ops = $this->camelcaseToUnderscore($ops);
+	    $query = QB::select()->from($this->table)->where($ops,"=",$args[0]);
+    
+	    return DB::fetch($query,$query->data);
+	}
 }

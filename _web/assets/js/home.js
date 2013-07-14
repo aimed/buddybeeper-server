c6cefac9d704617527a2ae0c98881fb77dea3402
aimed <revdemiat@gmail.com>
2013-07-14 22:53:20 +0200

    angular, angular, angular

--- _web/assets/js/home.js
+++ _web/assets/js/home.js
@@ -1,9 +1,44 @@
 bb.controller("event", ["$scope", function (scope) {
-	scope.showPeopleList = false;
-	scope.mode = "compact";
-	scope.isHost = function (event) { return event.host.id == scope.user.id; }
-	scope.hasVotedOn = function (item) { return item.votes.indexOf(scope.user.id) > -1; }
-	scope.togglePeopleList = function (bool) { scope.showPeopleList = bool; }
+	scope.showPeopleList  = false;
+	scope.limitPeopleList = 2;
+	scope.mode 			  = "compact";
+	scope.isHost 			= function () { return scope.event.host.id == scope.user.id; }
+	scope.hasVotedOn 		= function (item) { return item.votes.indexOf(scope.user.id) > -1; }
+	scope.togglePeopleList 	= function (bool) { 
+		scope.showPeopleList = bool; scope.limitPeopleList = bool ? scope.event.invites.length : 2; 
+	}
+	scope.toggleVote = function (item, event, type) {
+		if (scope.hasVotedOn(item,event,type)) {
+			item.votes.splice(item.votes.indexOf(scope.user.id),1);
+		} else {
+			item.votes.push(scope.user.id);
+		}
+	}
+	scope.addComment = function () {
+		scope.event.comments.push({text:scope.commentText,user:scope.user,id:scope.event.comments.length*2});
+		scope.commentText = "";
+	}
+	scope.addDate = function (date) {
+		date.votes = [scope.user.id];
+		scope.event.dates.push(date);
+	}
+	scope.addActivity = function (activity) {
+		activity.votes = [scope.user.id];
+		scope.event.activities.push(activity);
+	}
+	scope.invite = {
+		submit : function () {
+			scope.event.invites.push({
+				first_name:this.first_name,
+				last_name:this.last_name,
+				id:scope.event.invites.length+1
+			});
+			this.first_name = "";
+			this.last_name = "";
+			this.email = "";
+			scope.limitPeopleList = scope.event.invites.length;
+		}
+	}
 }]);
 
 bb.controller("landingPage", ["$scope","User", function (scope, user) {
--- _web/assets/js/static.js
+++ _web/assets/js/static.js
@@ -11,38 +11,53 @@ static.users = [
 	first_name    : "Max",
 	last_name     : "Appleseed",
 	profile_image : "/assets/img/default_user_image.png",
-	events		  : [{
-		id          : 1,
-		host		: {
-			id			  : 1,
-			first_name    : "Max",
-			last_name     : "Appleseed",
-			profile_image : "/assets/img/default_user_image.png"
-		},
-		title       : "Zombie hunting?",
-		description : "Hey guys, we haven't been zombie hunting in a while.",
-		invites     : [
-			{
+	events		  : [
+		{
+			id          : 1,
+			host		: {
 				id			  : 1,
 				first_name    : "Max",
 				last_name     : "Appleseed",
 				profile_image : "/assets/img/default_user_image.png"
-			}
-		],
-		dates       : [{start:new Date(),votes:[1]}],
-		activities  : [{name:"Zombie hunting",votes:[]}],
-		comments : [
-			{
-				user:{
+			},
+			title       : "Zombie hunting?",
+			description : "Hey guys, we haven't been zombie hunting in a while.",
+			invites     : [
+				{
 					id			  : 1,
 					first_name    : "Max",
 					last_name     : "Appleseed",
 					profile_image : "/assets/img/default_user_image.png"
 				},
-				text : "Greate idea!"
-			}
-		]
-	}]
+				{
+					id			  : 2,
+					first_name    : "Peter",
+					last_name     : "Bob",
+					profile_image : "/assets/img/default_user_image.png",
+				},
+				{
+					id			  : 3,
+					first_name    : "Alice",
+					last_name     : "Whothef",
+					profile_image : "/assets/img/default_user_image.png",
+				}
+				
+			],
+			dates       : [{start:new Date(),votes:[1]}],
+			activities  : [{name:"Zombie hunting",votes:[]},{name:"Eat brain",votes:[2]}],
+			comments : [
+				{
+					user:{
+						id			  : 1,
+						first_name    : "Max",
+						last_name     : "Appleseed",
+						profile_image : "/assets/img/default_user_image.png"
+					},
+					text : "Greate idea!"
+				}
+			]
+		}
+	]
 },
 {
 	id			  : 2,

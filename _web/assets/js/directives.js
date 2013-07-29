478c490509da14b5e6edbbbc7e3e22bbbb30d25b
aimed <revdemiat@gmail.com>
2013-07-29 16:59:41 +0200

    placeholder is no longer displayed in inlineedit

--- _web/assets/js/directives.js
+++ _web/assets/js/directives.js
@@ -1,18 +1,25 @@
 bb.directive("bgImage", function () {
-	return {
-		scope: {bgImage: "@"},
-		link : function (scope, elem, attrs) {
-			attrs.$observe("bgImage", function (val) {
-				elem.css({"background-image":"url(" + val + ")"});	
-			});
-		}
+return {
+	scope: {bgImage: "@"},
+	link : function (scope, elem, attrs) {
+		attrs.$observe("bgImage", function (val) {
+			elem.css({"background-image":"url(" + val + ")"});	
+		});
 	}
+}
 });
 
 bb.directive("inlineEdit", function () {
-	return {
-		replace : true,
-		template : '<div><div ng-hide="editWhen()">{{model ? model : placeholder}}</div><input ng-show="editWhen()" ng-model="model" placeholder="{{placeholder}}" /></div>',
-		scope : { editWhen : "&", model : "=editBind", placeholder : "@editPlaceholder"}
-	}
+return {
+	replace : true,
+	template : '<div><div ng-hide="editWhen()">{{model}}</div><input ng-show="editWhen()" ng-model="model" placeholder="{{placeholder}}" /></div>',
+	scope : { editWhen : "&", model : "=editBind", placeholder : "@editPlaceholder"}
+}
+});
+
+bb.directive("datepicker", function () {
+var obj = {
+	template : '<table><tr></tr></table>'
+}
+return function () {}
 });

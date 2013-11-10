62599bbea5ed48613e33ae8291a3e19f41524ac0
aimed <aimed@gmx.net>
2013-11-10 14:58:32 +0100

    whooohohooo

--- _web/index.php
+++ _web/index.php
@@ -115,7 +115,6 @@ $router->get("/_desktop/*", function (&$req, &$res) {
 
 $router->get("/*", function (&$req, &$res) {
 	// Check if mobile site was requested.
-	// some pointless comment
 	$env = substr($_SERVER["SERVER_NAME"], 0, 2) === "m." ?
 		"_mobile" :
 		"_desktop";
